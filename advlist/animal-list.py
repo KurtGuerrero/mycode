fbc5d166089f2e31fc022b92a9ccf5b6f0faae6e
Kurt Guerrero <KurtGuerrero@users.noreply.github.com>
2021-10-18 19:44:38 +0000

    About complex lists

---
+++ advlist/animal-list.py
@@ -0,0 +1,3 @@
+#!/usr/bin/env python3
+animal1 = ["Fox", "Fly", "Ant", "Bee", "Cod", "Cat", "Dog", "Yak", "Cow", "Hen", "Koi", "Hog", "Jay", "Kit"]
+print(animal1)
---
+++ listmethods/listmeth02.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+proto = ["ssh", "http", "https"]
+protoa = ["ssh", "http", "https"]
+print(proto)
+proto.append("dns") # this line will add "dns" to the end of our list
+#I want to sort proto
+print(protoa.sort())
+
+
+protoa.append("dns") # this line will add "dns" to the end of our list
+print(proto)
+proto2 = [ 22, 80, 443, 53 ] # a list of common ports
+print(type(proto2))
+proto.extend(proto2) # pass proto2 as an argument to the extend method
+print(proto)
+protoa.append(proto2) # pass proto2 as an argument to the append method
+print(protoa)
+
+
